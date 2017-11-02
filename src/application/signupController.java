e25316e0d4f0c5b3b40465c628db6000cf7ce050
Ahilya Sinha <ahilya16009@iiitd.ac.in>
2017-11-03 00:34:38 +0530

    Update signupController.java

--- src/application/signupController.java
+++ src/application/signupController.java
@@ -1,16 +1,44 @@
 package application;
 
+
+
 import javafx.collections.FXCollections;
+import javafx.event.*;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
 
 public class signupController {
 	public ChoiceBox<String> choices;
+	public TextField email, name;
+	public Label error;
+	public PasswordField password;
+	public Button button;
+	String a = "";
+
 
 	@FXML
 	public void initialize() {
 	    choices.getItems().removeAll(choices.getItems());
 	    choices.getItems().addAll("Admin", "Faculty", "Student");
 	    choices.getSelectionModel().select("Student");
+	    button.setOnAction(this::handleButtonAction);
+	    error.setVisible(false);
 	}
+
+	 private void handleButtonAction(ActionEvent event) {
+
+		 if(name.getText().equals("") || email.getText().equals("") || password.getText().equals("") ||choices.getValue().equals("")){
+			 error.setVisible(true);
+		 }
+		 else if(email.getText().endsWith("@iiitd.ac.in")==false){
+			 error.setVisible(true);
+		 }
+
+		 else{
+			 error.setVisible(false);
+			 a=a+name.getText()+","+email.getText()+","+password.getText()+","+choices.getValue();
+		     System.out.println(a);
+		 }
+
+	 }
 }
