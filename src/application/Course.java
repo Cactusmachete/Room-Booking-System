5e84d3c28128c71ecb78a9c1088726fd22f9c022
Rohan Chhokra <rohan16080@iiitd.ac.in>
2017-11-02 21:30:04 +0530

    Course basic layout

--- src/application/Course.java
+++ src/application/Course.java
@@ -1,5 +1,17 @@
 package application;
+import java.util.*;
 
 public class Course {
-
+	String name;
+	ArrayList<String> postcond;
+	ArrayList<String> prereq;
+	String instructor;
+	String[] timetable;
+	public Course(String name,ArrayList<String> postcond,ArrayList<String> prereq,String instructor,String[] timetable) {
+		this.name = name;
+		this.postcond = postcond;
+		this.prereq = prereq;
+		this.instructor = instructor;
+		this.timetable = timetable;
+	}
 }
