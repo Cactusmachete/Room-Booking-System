1430f4dcaa5a2764c3177a5fa8cc83399dd72311
Ahilya Sinha <ahilya16009@iiitd.ac.in>
2017-11-02 21:42:37 +0530

    Add files via upload

---
+++ src/application/Student.java
@@ -0,0 +1,35 @@
+package application;
+
+public class Student extends User {
+	TimeTable timetable;
+
+	public Student(String id, String pwd) {
+		super(id, pwd);
+	}
+
+	public void requestRoom(Request request){
+
+	}
+
+	public void searchCourse(String keywords){
+
+	}
+
+	public void addToTimetable(Course course){
+
+	}
+
+
+
+}
+
+
+class TimeTable{
+
+}
+class Request{
+
+}
+class Course{
+
+}
