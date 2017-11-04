c8b9b6d7f781df221d9363de4a0acf8e42081284
Ahilya Sinha <ahilya16009@iiitd.ac.in>
2017-11-05 02:18:11 +0530

    Update Room.java

--- src/application/Room.java
+++ src/application/Room.java
@@ -3,19 +3,28 @@ import java.io.*;
 
 
 public class Room implements Serializable{
+
+
+	private static final long serialVersionUID = 10L;
 	String name;
+    String slot;
+    String bookedBy;
+
 	int Capacity;
 	int[] Monday=new int[48];
 	int[] Tuesday= new int[48];
 	int[] Wednesday = new int[48];
 	int[] Thursday = new int[48];
 	int[] Friday = new int[48];
-	boolean status = false;
+	Boolean status = false;
+
 	public Room(String x,int y) {
 		this.name = x;
+		bookedBy = null;
+		this.slot = "all";
 		this.Capacity = y;
 	}
-	
+
 	public static void serialize(Room room) throws IOException  {
 		 ObjectOutputStream out = null;
 		 try {
@@ -31,20 +40,54 @@ public class Room implements Serializable{
 			 out.close();
 		 }
 	 }
-	
+
+	public static void main() {
+		Room first = new Room("C01",120);
+		Room second = new Room("C21",100);
+		try {
+			serialize(first);
+			serialize(second);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public int getCapacity(){
+		return this.Capacity;
+	}
+
+	public String getSlot(){
+		return (this.slot);
+	}
+
+
+	public Boolean isBooked(){
+		return this.status;
+	}
+
+	public String BookedBy(){
+		return this.bookedBy;
+	}
+
 	public static Room[] deserialize() {
 		Room[] list = new Room[2];
 		ObjectInputStream in1 = null;
 		try {
-			in1 = new ObjectInputStream(new FileInputStream("Room/c11.ser"));
+			in1 = new ObjectInputStream(new FileInputStream("Room/C21.ser"));
 			Room first = (Room)in1.readObject();
 			in1.close();
-			in1 = new ObjectInputStream(new FileInputStream("Room/c01.ser"));
+			in1 = new ObjectInputStream(new FileInputStream("Room/C01.ser"));
 			Room second = (Room)in1.readObject();
 			list[0] = first;
 			list[1] = second;
 			in1.close();
-			
+
 		}
 		catch(ClassNotFoundException e) {
 			e.printStackTrace();
@@ -54,4 +97,6 @@ public class Room implements Serializable{
 		}
 		return list;
 	}
+
+
 }
