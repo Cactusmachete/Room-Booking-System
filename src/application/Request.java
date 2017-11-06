da72ff4f06a937dd22a9e9b3dcefe3a05a1e46e5
Rohan Chhokra <rohan16080@iiitd.ac.in>
2017-11-07 03:26:58 +0530

    Added serialization and deserialization of Requests

---
+++ src/application/Request.java
@@ -0,0 +1,57 @@
+package application;
+import java.io.*;
+import java.util.*;
+public class Request implements Serializable {
+	private static final long serialVersionUID = 56L;
+	Student Requester;
+	boolean RequestStatus;
+	String purpose;
+	int Capacity;
+	public Request(Student Requester,String purpose,int capacity) {
+		this.Requester = Requester; 
+		this.purpose = purpose;
+		this.Capacity = capacity;
+		
+	}
+	public static void serialize(Request request) throws IOException  {
+		 ObjectOutputStream out = null;
+		 try {
+			 File inp = new File("Request/"+request.Requester.email_id+"/"+request.Requester.numofrequest+".ser");
+			 inp.createNewFile();
+			 out = new ObjectOutputStream(new FileOutputStream(inp, false));
+			 out.writeObject(request);
+		 }
+		 catch(IOException e) {
+			 e.printStackTrace();
+		 }
+		 finally {
+			 out.close();
+		 }
+	 }
+	public static ArrayList<Request> deserialize() {
+		ArrayList<Request> adminrequestlist = new ArrayList<Request>();
+		ObjectInputStream in = null;
+		try {
+			File hello = new File("Request/");
+			String[] list = hello.list();
+			for(int i = 0;i<list.length;i++) {
+				File innerhello = new File("Reqest/"+list[i]);
+				String[] innerlist = innerhello.list();
+				for(int j = 0;j<innerlist.length;j++){
+					in = new ObjectInputStream(new FileInputStream(""+j));
+					Request yay = (Request) in.readObject();
+					adminrequestlist.add(yay);
+				}
+			}
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		return adminrequestlist;
+		
+	}
+	
+}
--- src/application/Student.java
+++ src/application/Student.java
@@ -1,15 +1,22 @@
 package application;
 import java.util.*;
+import java.io.*;
 public class Student extends User {
 	private static final long serialVersionUID = 3L;
 	ArrayList<String> CourseList;
+	ArrayList<Request> RequestList;
+	int numofrequest = 0;
 	public Student(String id, String pwd,String name) {
 		super(id, pwd,name);
+		File Folder = new File("Request/"+id);
+		Folder.mkdir();
 	}
 
 	public void addcourse(String Coursename) {
 		CourseList.add(Coursename);
 	}
+	
+	
 
 
 
