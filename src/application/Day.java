497462b865d9ea9995b6d4ba07d500c4c63f3fc2
Ahilya Sinha <ahilya16009@iiitd.ac.in>
2017-11-11 18:47:53 +0530

    Update Day.java

--- src/application/Day.java
+++ src/application/Day.java
@@ -7,6 +7,38 @@ public class Day implements Serializable {
 	private static final long serialVersionUID = 500L;
 	String date;
 	int[] Status = new int[48];
+	Boolean status = false;
+	String bookedBy="";
+	String Purpose="";
+
+	int[] slots = new int[48];
+
+
+	public Day(String date) {
+		this.date = date;
+	}
+
+	public String toString() {
+		 return this.date;
+	}
+
+	public Boolean isBooked(){
+		return this.status;
+	}
+
+	public void book(String by, String purpose){
+		this.bookedBy = by;
+		this.Purpose = purpose;
+		this.status=true;
+	}
+	public void cancel() {
+		this.bookedBy="";
+		this.status=false;
+		this.Purpose="";
+
+	}
+
+
 	
 	public Day(String date) {
 		this.date = date;
@@ -59,6 +91,8 @@ public class Day implements Serializable {
 			Status[i] = 0;
 		}
 	}
+	
+	
 
 }
 
